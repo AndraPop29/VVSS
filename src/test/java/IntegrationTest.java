975a6e442c5df0c6ff5c071ca84c1b404068a144
Andra Pop <andra.pop@halcyonmobile.com>
2018-05-07 18:03:54 +0300

    he3

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -117,7 +117,7 @@ public class IntegrationTest {
     }
 
     @Test
-    public void BigBangAllValid() { // valid member, valid entry
+    public void testBigBangAllValid() { // valid member, valid entry
         testAddMemberValid();
         testAddValidEntry();
         testGetEntriesForValidId();
